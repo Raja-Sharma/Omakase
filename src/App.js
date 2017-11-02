d8f6aa99840c5dba9dcf6a6d524b1ee95cdc1f73
Raja Sharma <rajasharma55@gmail.com>
2017-11-02 17:09:29 -0400

    Add src directory

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Omakase
+      </Text>
+    </View>
+  );
+};
+
+export default App;
