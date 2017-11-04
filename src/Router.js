5cad3023b3f03a3764bb6ebbf675f7e7dfc9ed16
Raja Sharma <rajasharma55@gmail.com>
2017-11-04 16:27:12 -0400

    Finish Clarifai API call setup. Create temporary display component to test image recognition.

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Scene, Stack, Router } from 'react-native-router-flux';
 import Home from './containers/Home';
 import AppCamera from './containers/AppCamera';
+import Display from './containers/Display';
 
 
 const RouterComponent = () => {
@@ -10,6 +11,7 @@ const RouterComponent = () => {
       <Stack key="root">
         <Scene title="Welcome" key="home" component={Home} initial />
         <Scene title="Camera" key="appCamera" component={AppCamera} />
+        <Scene title="Display" key="display" component={Display} />
       </Stack>
     </Router>
   )
--- src/actions/index.js
+++ src/actions/index.js
@@ -17,15 +17,18 @@ export function goToCamera() {
 }
 
 export function fetchIngredients(data) {
-  ReadImageData.readImage(data.path, (imageBase64) => {
-    clarifaiCall.models.predict("bd367be194cf45149e75f01d59f77ba7", {base64: imageBase64})
-      .then(response => {
-        console.log('action fetchIngredients api response', response.outputs[0].data.concepts);
-        return {
-          type: 'INGREDIENTS',
-          payload: response.outputs[0].data.concepts
-        }
-      })
-      .catch(err => console.log(err))
-  });
+  Actions.display()
+  function filterByValue(obj) {
+    return obj.value > 0.80
+  }
+  return (dispatch) => {
+    ReadImageData.readImage(data.path, (imageBase64) => {
+      clarifaiCall.models.predict("bd367be194cf45149e75f01d59f77ba7", {base64: imageBase64})
+        .then(response => {
+          dispatch({
+            type: 'INGREDIENTS',
+            payload: response.outputs[0].data.concepts.filter(filterByValue) })
+        })
+    })
+  }
 }
